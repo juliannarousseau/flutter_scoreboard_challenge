bc32291ad4513568da160817a7c221a6259e9613
Juliana Rousseau <juliana.salafia1@gmail.com>
2026-01-20 18:06:58 -0300

    feat: implement ScoreboardRepositoryImpl and define ScoreboardRepositoryInterface

---
+++ lib/src/data/repositories/scoreboard_repository_impl.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_scoreboard_challenge/src/domain/repositories/scoreboard_repository_interface.dart';
+
+class ScoreboardRepositoryImpl implements ScoreboardRepositoryInterface {}
